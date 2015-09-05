88ae690e7a3d1a98861182e394fa52592855b16e
Vadim Manaenko <vadim.razorq@gmail.com>
2015-09-05 19:39:39 +0300

    Some refactoring, nothing really important

--- app/src/main/java/com/vourheyapps/randomizer/CoinFlipActivity.java
+++ app/src/main/java/com/vourheyapps/randomizer/CoinFlipActivity.java
@@ -5,12 +5,16 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by vourhey on 8/16/15.
  */
 public class CoinFlipActivity extends Activity {
     private Drawable[] coin;
+    private String[] headsOrTails;
+    private TextView headsOrTailsText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +24,18 @@ public class CoinFlipActivity extends Activity {
         coin[0] = getResources().getDrawable(R.drawable.coin_heads);
         coin[1] = getResources().getDrawable(R.drawable.coin_tails);
 
+        headsOrTails = new String[2];
+        headsOrTails = getResources().getStringArray(R.array.string_heads_or_tails);
+
+        headsOrTailsText = (TextView) findViewById(R.id.coinHeadOrTailsText);
         final ImageView coinImage = (ImageView) findViewById(R.id.coinflipImage);
         coinImage.setImageDrawable(coin[0]);
         coinImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                coinImage.setImageDrawable(coin[MainActivity.random.nextInt(2)]);
+                int c = MainActivity.random.nextInt(2);
+                coinImage.setImageDrawable(coin[c]);
+                headsOrTailsText.setText(headsOrTails[c]);
             }
         });
     }
